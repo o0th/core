c5811064522f0b729a928eaaa108306250610d43
o0th <o0th@pm.me>
2024-05-28 10:57:49 +0200

    feat: indentblankline

--- home/o0th/.config/nvim/lua/plugins/lsp.lua
+++ home/o0th/.config/nvim/lua/plugins/lsp.lua
@@ -54,7 +54,7 @@ return {
 
     require('mason').setup()
     require('mason-lspconfig').setup({
-      ensure_installed = { 'lua_ls' },
+      ensure_installed = { 'lua_ls', 'vimls' },
       handlers = {
         function(server_name)
           require('lspconfig')[server_name].setup({})
