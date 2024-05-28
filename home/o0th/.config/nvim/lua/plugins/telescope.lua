64b456d53dad61f21b0c3cb592c244eff05008a6
o0th <o0th@pm.me>
2024-05-28 10:13:20 +0200

    fix: format

--- home/o0th/.config/nvim/lua/plugins/telescope.lua
+++ home/o0th/.config/nvim/lua/plugins/telescope.lua
@@ -1,18 +1,19 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	init = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-	end,
-	config = {
-		pickers = {
-			find_files = {
-				theme = 'ivy'
-			}
-		}
-	},
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.6',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  init = function()
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+  end,
+  config = {
+    pickers = {
+      find_files = {
+        theme = 'ivy',
+      },
+    },
+  },
 }
